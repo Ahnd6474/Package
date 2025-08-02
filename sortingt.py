6a38df44c43e6aaead2e0d5a86bd97ee83b22dcf
Danny <ahnd6474@gmail.com>
2025-08-02 15:11:18 +0900

    Update and rename Heapsort.py to sortingt.py

--- sortingt.py
+++ sortingt.py
@@ -3,8 +3,14 @@ import sys
 input = sys.stdin.readline
 write = sys.stdout.write
 
+def hp_sort(l:[]):
+    h=Heap(data=l, key = lambda x, y: x<y)
+    for i in range(len(l)):
+        write(str(h.pop())+'\n')
 n = int(input())
 l=[0]*n
 for i in range(n):
     l[i]=int(input())
 hp_sort(l)
+
+
