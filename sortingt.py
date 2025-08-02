868a966477bf58d0242b70035837bfeb95e9f181
Danny <ahnd6474@gmail.com>
2025-08-02 15:28:19 +0900

    Update sortingt.py

--- sortingt.py
+++ sortingt.py
@@ -13,4 +13,25 @@ for i in range(n):
     l[i]=int(input())
 hp_sort(l)
 
+def selection_sort(l,ret=False):
+    n=len(l)
+    for i in range(n):
+        min=i
+        for j in range(n-i):
+            min= i+j if l[min]>l[i+j] else min
+        l[i],l[min]=l[min],l[i]
+    return l
+
+import sys
+input = sys.stdin.readline
+write = sys.stdout.write
+
+n = int(input())
+l=[0]*n
+for i in range(n):
+    l[i]=int(input())
+l=selection_sort(l)
+for i in l:
+    write(str(i))
+    write('\n')
 
